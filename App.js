d5903a66b20e0a276903b4bb3b1f6ab6430f4cbc
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-05 16:05:11 +0300

    Add logo

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+const logo = require("../assets/icon.png");
 // import NavBar from './components/NavBar';
-import Menu from './components/Pages/Menu';
+// import Menu from './components/Pages/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Menu />
+      <Text>my First Project Mobile App</Text>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
       <StatusBar style="auto" />
     </View>
   );
