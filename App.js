8545825d27c43ff1c992aa41c3918607ba3b44eb
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-05 16:09:12 +0300

    remove components

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-const logo = require("../assets/icon.png");
-// import NavBar from './components/NavBar';
-// import Menu from './components/Pages/Menu';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>my First Project Mobile App</Text>
-      <Image source={logo} style={{ width: 305, height: 159 }} />
+
       <StatusBar style="auto" />
     </View>
   );
